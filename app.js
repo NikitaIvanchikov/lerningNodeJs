93adc87cbf93739e62fef153f07e4dfebed040c9
NikitaIvanchikov <n.o.ivanhikov@gamil.com>
2021-02-05 13:44:17 +0200

    first app created

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log ('Hello World');
+
+let a = 5;
+
+console.log (` a = ${a}`);
+
+test = () => {
+	console.log ('123345');
+}
+
+test();
+
+const b = () => {
+	let a = 5;
+	let h = 10;
+
+	let sum = a + h;
+	return sum;
+
+}
+console.log(b());
